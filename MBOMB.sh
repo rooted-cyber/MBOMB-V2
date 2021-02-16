90a62fb78f8513575da0164dedc2dde8f450afb6
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-02-16 11:20:41 +0530

    Add files via upload

--- MBOMB.sh
+++ MBOMB.sh
@@ -1,66 +1,85 @@
-lal="\033[91m"
-bulu="\033[94m"
-hara="\033[92m"
-pila="\033[93m"
-image="figlet"
-rang="toilet -f term -F gay"
-$image MBOMB | $rang
-echo
-cd ~/MBOMB-V2
-#cp -f req* $PREFIX
-#chmod 777 $PREFIX/req*
-cd $PREFIX/bin
-if [ -e figlet ];then
-echo
-else
-apt update
-apt upgrade
-apt install figlet
-apt install toilet
-apt install curl
-apt install python
-cd ~/MBOMB-V2
-pip install -r requiremen*
-clear
-fi
-if [ -e M ];then
-echo
-else
-cd ~/MBOMB-V2
-pip install -r requiremen*
-echo "#!/data/data/com.termux/files/usr/bin/sh" >> $PREFIX/bin/M
-echo "cd ~/MBOMB-V2" >> $PREFIX/bin/M
-echo "bash MBOMB.sh" >> $PREFIX/bin/M
-chmod 777 $PREFIX/bin/M
-printf "\n\n Now you can start this command :- M "
-sleep 3
-read
-fi
-clear
-$image TBomb | $rang
-call () {
-	cd ~/MBOMB-V2
-	python3 .bomb.py call
+setup() {
+	cd ~/MBOMB-V2/files
+	dpkg -i Random.deb
+	random
+	printf "\n Updating package..\n"
+	apt update
+	apt upgrade
+	apt-get update
+	apt-get upgrade
+	for i in nodejs python figlet curl toilet php; do
+	if [ -e $PREFIX/bin/$i ];then
+	echo
+	else
+	random
+	printf "\n Installing $i\n\n"
+	random
+	apt install $i || apt install --fix-broken
+	apt reinstall $i
+	clear
+	sleep 1
+	random
+	printf "\n Successfully installed $i\n\n"
+	fi
+	done
+	cd ~/MBOMB-V2/files
+	dpkg -i Font.deb
+	if [ -e $PREFIX/bin/python ];then
+	cd ~/MBOMB-V2/php
+	php install.php
+	else
+	cd ~/MBOMB-V2/php
+	php package.php
+	fi
+	echo "#!/data/data/com.termux/files/usr/bin/sh" >> $PREFIX/bin/M
+	echo "cd ~/MBOMB-V2" >> $PREFIX/bin/M
+	echo "bash MBOMB.sh" >> $PREFIX/bin/M
+	chmod 777 $PREFIX/bin/M
+	printf "\n\n Now you can start this command :- M "
+	read
 	}
-	sms () {
+	banner() {
+		if [ -e $PREFIX/share/figlet/font.flf ];then
 		cd ~/MBOMB-V2
-		python3 .bomb.py
+		cd php
+		php banner.php
+		cd ~/MBOMB-V2
+		cd javascript
+		node enter.js
+		fi
 		}
-		MB () {
-			printf "\nInvalid Option !!!\n"
-			sleep 0.60
-			M
+		fol() {
+			cd ~/MBOMB-V2/php
 			}
-echo
-printf "$hara	[ 1 ]$pila Call bomber\n"
-printf "$hara	[ 2 ]$pila Sms bomber\n"
-printf "$hara	[ 3 ]$lal Exit\n"
-echo
-printf %s"Select "
-read a
-case $a in
-1)call ;;
-2)sms ;;
-3)exit 0 ;;
-*)MB ;;
-esac
+		sms() {
+			fol
+			php sms.php
+			}
+			call () {
+				fol
+				php call.php
+				}
+		menu () {
+			banner
+			cd ~/MBOMB-V2
+			cd javascript
+			node menu.js
+			echo -e -n "\033[1;93m Mbomb >> :\033[0m "
+			read a
+			case $a in
+			1) sms ;;
+			2) call ;;
+			3) setup ;;
+			4) exit ;;
+			*)menu ;;
+			esac
+			}
+			check() {
+				if [ -e ~/MBOMB-V2 ];then
+				menu
+				else
+				cd ~
+				git clone https://github.com/rooted-cyber/MBOMB-V2
+				fi
+				}
+				check
